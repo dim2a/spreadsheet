33a9c988d1ca62bac60c439e7edd2122f25e55ad
Dmitry Buinitsky <dmitry.buinitsky@intexsoft.by>
2025-10-14 14:07:43 +0300

    one table

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import formula from '@jspreadsheet/formula';
 import { useEffect, useRef } from 'react';
 import tableSettings from './tableData.json';
 // import tableSettings from '../calcData.json';
+import { Table } from './Table';
 
 const INIT_WORKSHEET_CONFIG: jspreadsheet.Worksheet = {
   search: false,
@@ -27,47 +28,23 @@ const INIT_WORKSHEET_CONFIG: jspreadsheet.Worksheet = {
 
 function App() {
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const instanceRef = useRef<any>(null);
 
   useEffect(() => {
     // License + community formula engine
     jspreadsheet.setLicense('YTM2MmYxNWZiOTBlMGM5NjU0MGJhMTFjMGNmNzNlODM5ZWVjODJjN2VmNTNkYzNiYTVmZWM4ZTYxYzc5YzUxZDQ0NzdkYWI4OGQyYTBlZDk3MmI4NWQxNGY4ZDc1YTlkM2U2ODc4NWVhZmNkOGQyYzJiZDExYWMzNGEzN2Y5NjYsZXlKamJHbGxiblJKWkNJNklpSXNJbTVoYldVaU9pSktjM0J5WldGa2MyaGxaWFFpTENKa1lYUmxJam94TnpZM01USTROREF3TENKa2IyMWhhVzRpT2xzaWFuTndjbVZoWkhOb1pXVjBMbU52YlNJc0ltTnZaR1Z6WVc1a1ltOTRMbWx2SWl3aWFuTm9aV3hzTG01bGRDSXNJbU56WWk1aGNIQWlMQ0ozWldJaUxDSnNiMk5oYkdodmMzUWlMQ0prWlhZdGMyTnZjR1Y0TG5CeWFXMWxiR1Z3YUdGdWRITXVaR1VpWFN3aWNHeGhiaUk2SWpNMElpd2ljMk52Y0dVaU9sc2lkamNpTENKMk9DSXNJblk1SWl3aWRqRXdJaXdpZGpFeElpd2lZMmhoY25Seklpd2labTl5YlhNaUxDSm1iM0p0ZFd4aElpd2ljR0Z5YzJWeUlpd2ljbVZ1WkdWeUlpd2lZMjl0YldWdWRITWlMQ0pwYlhCdmNuUmxjaUlzSW1KaGNpSXNJblpoYkdsa1lYUnBiMjV6SWl3aWMyVmhjbU5vSWl3aWNISnBiblFpTENKemFHVmxkSE1pTENKamJHbGxiblFpTENKelpYSjJaWElpTENKemFHRndaWE1pWFN3aVpHVnRieUk2ZEhKMVpYMD0=');
     jspreadsheet.setExtensions({ formula });
-
-    if (instanceRef.current) return;
-
-    const init = () => {
-      if (!containerRef.current) return;
-      const rect = containerRef.current.getBoundingClientRect();
-      if (rect.width < 200 || rect.height < 200) {
-        requestAnimationFrame(init);
-        return;
-      }
-
-      instanceRef.current = jspreadsheet(containerRef.current, {
-        tabs: false,
-        worksheets: [{
-          ...INIT_WORKSHEET_CONFIG,
-          ...tableSettings.tableSettings,
-          worksheetName: 'virtual'
-        }]
-      } as any);
-    };
-
-    requestAnimationFrame(init);
   }, []);
 
   return (
     <div>
       <div style={{ width: '80vw', height: '80vh' }}>
-        <div ref={containerRef} style={{ width: '100%', height: '100%' }} />
+        <div ref={containerRef} style={{ width: '100%', height: '100%' }} >
+          <Table
+            tableSettings={tableSettings}
+          />
+        </div>
       </div>
-
-      <button onClick={() => {
-        console.log(containerRef.current?.spreadsheet?.getConfig().worksheets[0]);
-      }}>
-        getConfig
-      </button>
+      <span>hello</span>
     </div>
   )
 }
