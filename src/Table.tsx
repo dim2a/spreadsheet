5a94ed37eb5f7f8f6c4fd47c2e1646b0f49285b8
Dmitry Buinitsky <dmitry.buinitsky@intexsoft.by>
2025-10-14 14:07:55 +0300

    one table

---
+++ src/Table.tsx
@@ -0,0 +1,71 @@
+import jspreadsheet from "jspreadsheet";
+import { useEffect, useRef } from "react";
+
+const INIT_WORKSHEET_CONFIG: jspreadsheet.Worksheet = {
+  search: false,
+  columnDrag: false,
+  columnSorting: false,
+  defaultColWidth: 100,
+  rowResize: true,
+  defaultColAlign: 'left',
+  allowInsertRow: true,
+  allowManualInsertRow: false,
+  allowManualInsertColumn: false,
+  allowInsertColumn: true,
+  allowDeleteRow: true,
+  allowDeleteColumn: true,
+  allowRenameColumn: false,
+  tableHeight: 600,
+  tableWidth: 1200,
+  filters: false,
+  tableOverflow: true,
+};
+
+export const Table = ({ tableSettings }: { tableSettings: { [key: string]: any } }) => {
+  const jssRef = useRef<
+    (HTMLDivElement & { spreadsheet: jspreadsheet.spreadsheetInstance }) | null
+  >(null);
+
+
+  useEffect(() => {
+    if (jssRef.current?.spreadsheet) {
+      jspreadsheet.destroy(jssRef.current);
+      console.log('init destroy');
+      
+    }
+
+        console.log('initSpreadsheet');
+        
+      if (!jssRef.current) {
+        console.error('Container element not found');
+        return;
+      }
+
+      if (tableSettings) {
+        console.log({tableSettings});
+        
+try {
+        jspreadsheet(jssRef.current, {
+          about: 'calculation',
+          worksheets: [{
+            ...INIT_WORKSHEET_CONFIG,
+            ...tableSettings,
+          }],
+        });
+        
+        console.log('Spreadsheet initialized successfully');
+      } catch (error) {
+        console.error('Error initializing spreadsheet:', error);
+      }
+      }
+      
+
+  }, [tableSettings]);
+
+  return (
+    <div className="table">
+      <div ref={jssRef} className="table-container"></div>
+      <span>ping</span>
+    </div>
+  );
+};
